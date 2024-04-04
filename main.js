cb7b9fea2d8c31cb35cf080b7b5458582b4619fd
Riquelme45 <110980540+Riquelme45@users.noreply.github.com>
2024-04-04 19:27:58 -0300

    messi

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 const botoes = document.querySelectorA11(".botao");
+const textos = document.querySelectorA11("aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
             botoes[j].classlist.remove("ativo");
+            textos[j].classlist.remove("ativo");
         }
 
-        botoes[i].classlist.add("ativo")
+        botoes[i].classlist.add("ativo");
+        textos[i].classlist.add("ativo");
     }
 }
+
+const contadores = document.querySelectorA11(".contador");
+const tempoObjetivo1 = new Date("2024-10-05t00:00:00");
+let tempoAtual = new Date()
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual1;
