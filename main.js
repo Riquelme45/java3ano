b7b8b677fb064191b1bf806ef826243d14228813
Riquelme45 <110980540+Riquelme45@users.noreply.github.com>
2024-04-04 21:45:56 -0300

    vv

--- main.js
+++ main.js
@@ -22,14 +22,6 @@ const tempoObjetivo1 = new Date("2024-12-15t00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-function atualizaCronometro(){
-for (let i=0; i<contadores.length;i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);
-}
-}
-
-atualizaCronometro();
-setInterval(atualizaCronometro,1000);
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
@@ -42,8 +34,23 @@ function calculaTempo(tempoObjetivo){
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
+    if (tempoFinal > 0){
+        return dias +" dias "+ horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+    } else {
+        return "prazo Finalizado";
+    }
+   
+    }
     
-    return dias +" dias "+ horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);
+    }
+ }
+ 
+   function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+   }
 
-}
-
+   comecaCronometro();
